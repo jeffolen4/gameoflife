7e4a3d7f6db7f83a2e239d69f0a338fea38159ae
LEARN01 <learn01@LEARN01.local>
2015-01-21 11:47:27 -0800

    Game of life complete

--- js/life-game2-ui.js
+++ js/life-game2-ui.js
@@ -1,6 +1,9 @@
 $(document).ready( function () {
 
+
 var matrix = appLifeGame.matrix;
+var canvas = $("#life-canvas");
+var start = $("#start");
 
 var Life = {};
 
@@ -9,10 +12,17 @@ Life.X = 400;
 Life.Y = 400;
 Life.WIDTH = Life.X / Life.CELL_SIZE;
 Life.HEIGHT = Life.Y / Life.CELL_SIZE;
-Life.DELAY = 500;
+Life.DELAY = 200;
 Life.STOPPED = 0;
 Life.RUNNING = 1;
 
+Life.minimum = 2;
+Life.maximum = 3;
+Life.spawn = 3;
+
+Life.state = Life.STOPPED;
+Life.generations = 200;
+Life.currentGeneration = 0;
 
 var lifeCanvas = $("#life-canvas");
 lifeCanvas.css("height",Life.Y);
@@ -20,92 +30,184 @@ lifeCanvas.css("width", Life.X);
 
 var ctx = document.getElementById("life-canvas").getContext("2d");
 
-Life.minimum = 2;
-Life.maximum = 3;
-Life.spawn = 3;
-
-Life.state = Life.STOPPED;
-Life.interval = null;
-
 // build initial matrix ( all dead )
 Life.grid = matrix(Life.HEIGHT, Life.WIDTH, false);
+Life.initialState = Life.grid;
 
 Life.counter = 0;
 
 // set initial state
-Life.grid[15][15] = true;
-Life.grid[16][18] = true;
-Life.grid[17][14] = true;
-Life.grid[17][15] = true;
-Life.grid[17][18] = true;
-Life.grid[17][19] = true;
-Life.grid[17][20] = true;
+function setDefaultInitialState() {
+  Life.grid[25][15] = true;
+  Life.grid[26][18] = true;
+  Life.grid[27][14] = true;
+  Life.grid[27][15] = true;
+  Life.grid[27][16] = true;
+  Life.grid[27][17] = true;
+  Life.grid[27][18] = true;
+}
 
 function evaluateNeighbors ( x, y ) {
   // subtract out current position before evaluating grid
-  var total = ( Life.grid[x][y] ) ? 0 : -1;
+  var total = ( Life.grid[x][y] ) ? -1 : 0;
 
+  // evaluate all neighbors to find count of living
   for ( var h=-1; h <= 1; h++ ) {
     for ( v=-1;  v <= 1; v++ ) {
-      if ( !(Life.grid[ x + h ][ y + v ]) ) {
-        total++;
+      if ( x + h >= 0
+          && y + v >= 0
+          && x + h < Life.WIDTH
+          && x + v < Life.HEIGHT ) {
+        if ( (Life.grid[ x + h ][ y + v ]) ) {
+          total++;
+        };
       };
     };
   };
+  return total;
 }
 
+// evaluate all cells and build new matrix for next generation
 function nextGeneration () {
+
+  var changes = false;
+
   Life.nextGrid = matrix(Life.HEIGHT, Life.WIDTH, false);
+
   for ( var h=0; h < Life.WIDTH; h++ ) {
     for ( var v=0; v < Life.HEIGHT; v++ ) {
-      var deadCount = evaluateNeighbors( h, v )
-      var aliveCount = 8 - deadCount;
+
+      // get the number of living neighbors
+      var aliveCount = evaluateNeighbors( h, v )
+
       switch (true) {
-        case ( Life.grid[h][v] && aliveCount <=2 ) :
+        // Live cell dies if less than 2 live neighbors
+        case ( Life.grid[h][v] && aliveCount < Life.minimum ) :
           Life.nextGrid[h][v] = false;
+          changes = true;
           break;
-        case ( Life.grid[h][v] && aliveCount > 3) :
+        // Live cell dies if more than 3 live neighbors
+        case ( Life.grid[h][v] && aliveCount > Life.maximum ) :
           Life.nextGrid[h][v] = false;
+          changes = true;
           break;
-        case ( Life.grid[h][v] && ( aliveCount === 2 || aliveCount === 3 ) ) :
+        // Live cell continues to next generation if
+        //   exactly 2 or 3 living neighbors
+        case ( Life.grid[h][v] &&
+                ( aliveCount === Life.minimum || aliveCount === Life.maximum ) ) :
           Life.nextGrid[h][v] = true;
           break;
-        case ( !(Life.grid[h][v]) && aliveCount === 3 ) :
+        // Dead cell regenerates if exactly 3 living neighbors
+        case ( !(Life.grid[h][v]) && aliveCount === Life.maximum ) :
           Life.nextGrid[h][v] = true;
+          changes = true;
           break;
       }
     }
   }
-  return Life.nextGrid;
+  // return the updated
+  if ( changes ) {
+    return Life.nextGrid;
+  } else {
+    return false;
+  }
 }
 
+function renderCell( currentState, x, y ) {
+  if ( currentState ) {
+    ctx.fillStyle = "white";
+  } else {
+    ctx.fillStyle = "black";
+  }
+  ctx.fillRect( (x*Life.CELL_SIZE), (y*Life.CELL_SIZE), 4, 4 );
+
+}
 
 function renderGrid( currentGrid ) {
+
   for ( var h=0; h < Life.WIDTH; h++ ) {
     for ( var v=0; v < Life.HEIGHT; v++ ) {
-      ctx.rect( (h*Life.CELL_SIZE), (v*Life.CELL_SIZE), 4, 4 );
-      if ( currentGrid[h][v] ) {
-        ctx.fillStyle = "white";
-      } else {
-        ctx.fillStyle = "black";
-      }
-      ctx.fill();
+      renderCell( currentGrid[h][v], h, v);
     };
   };
 
 }
 
 function next () {
-  Life.grid = nextGeneration( Life.grid );
-  renderGrid( Life.grid );
+  // increment the generation
+  Life.currentGeneration++;
+  // if we have reached the set number of generations
+  //   then clear the interval
+  if ( Life.currentGeneration >= Life.generations ) {
+    clearInterval( Life.interval );
+  } else {
+    Life.grid = nextGeneration( Life.grid );
+    // nextGeneration will return false if nothing changed
+    //  if that occurs then clear the interval.
+    if ( !(Life.grid) ) {
+      clearInterval( Life.interval );
+    } else {
+      // otherwise re-render the grid
+      renderGrid( Life.grid );
+    }
+  }
 }
+
+// click on the canvas to set the initial state
+canvas.click( function (event) {
+
+  var x = Math.floor(event.offsetX / Life.CELL_SIZE);
+  var y = Math.floor(event.offsetY / Life.CELL_SIZE);
+
+  // toggle current state of selected cell
+  Life.grid[x][y] = !(Life.grid[x][y]);
+
+  // render only that cell
+  renderCell( Life.grid[x][y], x, y );
+
+  return false;
+})
+
+start.click( function (e) {
+  // save the current initial state to allow for reset
+  Life.initialState = Life.grid;
+  // reset the current generation to zero
+  Life.currentGeneration = 0;
+  // render the initial grid
+  renderGrid( Life.grid );
+  // set the interval for subsequent generations
+  Life.interval = setInterval( next, Life.DELAY );
+})
+
+$("#default").click( function (e) {
+  // reset to blank grid
+  Life.grid = matrix(Life.HEIGHT, Life.WIDTH, false);
+  // set the default initial state
+  setDefaultInitialState();
+  // render the grid
+  renderGrid( Life.grid );
+})
+
+$("#reset").click( function (e) {
+  // reset to blank grid
+  Life.grid = Life.initialState;
+  renderGrid( Life.grid );
+})
+
+$("#clear").click( function (e) {
+  // reset to blank grid
+  Life.grid = matrix(Life.HEIGHT, Life.WIDTH, false);
+  renderGrid( Life.grid );
+})
+
+$("#stop").click( function () {
+  Life.currentGeneration = Life.generations;
+})
+
 
 // initialize display;
 ctx.rect( 0, 0, Life.X, Life.Y );
 ctx.fillStyle = "black";
 ctx.fill();
-
-// render the initial grid
-renderGrid( Life.grid );
 
 })
